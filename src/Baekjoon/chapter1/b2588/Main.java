620981392c506887ac46153d3f9f831f87839fd4
leeesu <lsu0314@gmail.com>
2022-12-26 23:03:30 +0900

    codeUp : 100제 221226 #2

---
+++ src/Baekjoon/chapter1/b2588/Main.java
@@ -0,0 +1,11 @@
+package Baekjoon.chapter1.b2588;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%d\n",472*5);
+        System.out.printf("%d\n",472*80);
+        System.out.printf("%d\n",472*300);
+        System.out.printf("%d\n",472*385);
+
+    }
+}
